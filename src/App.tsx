89b53797e5b97c0278ec3833c480df99739e9277
jiyuan.zhang <jiyuan.zhang@bytedance.com>
2025-02-02 11:51:57 -0800

    Add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
-import './App.css'
+import { Routes, Route, Link } from 'react-router';
+import TicTacToec from "./pages/tic-tac-toe/tic-tac-toe.tsx";
 
-function App() {
-
+const App = () => {
   return (
-    <div className="bg-blue-500 p-4">
-        App Page
-    </div>
-  )
-}
+      <>
+        <h1>React Router</h1>
 
-export default App
+        <Navigation />
+
+        <Routes>
+          <Route path="home" element={<Home />} />
+          <Route path="users" element={<Users />} />
+          <Route path="tic" element={<TicTacToec />} />
+        </Routes>
+      </>
+  );
+};
+
+const Home = () => {
+  return (
+      <main style={{ padding: '1rem 0' }}>
+        <h2>Home</h2>
+      </main>
+  );
+};
+
+const Users = () => {
+  return (
+      <main style={{ padding: '1rem 0' }}>
+        <h2>Users</h2>
+      </main>
+  );
+};
+
+const Navigation = () => {
+  return (
+      <nav
+          style={{
+            borderBottom: "solid 1px",
+            paddingBottom: "1rem",
+          }}
+      >
+        <Link to="/home">Home</Link>
+        <Link to="/users">Users</Link>
+        <Link to="/tic">Tic</Link>
+      </nav>
+  );
+};
+
+export default App;
---
+++ src/pages/tic-tac-toe/tic-tac-toe.tsx
@@ -0,0 +1,11 @@
+
+
+function TicTacToe() {
+    return (
+        <div>
+            tic-tac-toe
+        </div>
+    )
+}
+
+export default TicTacToe
